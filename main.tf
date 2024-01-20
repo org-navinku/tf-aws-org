7eeb1956e33090f2e2a6309ada705dfade97de73
Navin Kumar <nk_kumar@outlook.com>
2024-01-20 15:01:57 +0530

    files updated

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "navstatefiles"
-    key     = "navstatefiles/tfstatefile/tfawsorg/terraform.tfstate"
+    bucket  = "bucketname"
+    key     = "bucket/path/terraform.tfstate"
     region  = "us-west-2"
     encrypt = true
     # dynamodb_table = "optional-dynamodb-lock-table-name"
